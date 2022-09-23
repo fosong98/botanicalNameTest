68f2c3e987c223ee24330d51a6cb1c59ef965d01
fosong98 <fosong98@gmail.com>
2022-09-23 22:02:50 +0900

    <feat> : init
    * 프로젝트 시작!

---
+++ src/main/java/com/botanicalNameTest/BotanicalNameTestApplication.java
@@ -0,0 +1,13 @@
+package com.botanicalNameTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotanicalNameTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotanicalNameTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/botanicalNameTest/BotanicalNameTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botanicalNameTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotanicalNameTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
